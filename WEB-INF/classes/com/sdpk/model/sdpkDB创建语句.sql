a6bb6d584c31f6a70be368ab42c92fc812ea9d0a
lieo <lieoluntan@163.com>
2017-12-18 14:20:39 +0800

    合并版本V2+12月18日+谢平平  （标志版本V2）
    Signed-off-by: lieo <lieoluntan@163.com>

--- WEB-INF/classes/com/sdpk/model/sdpkDB创建语句.sql
+++ WEB-INF/classes/com/sdpk/model/sdpkDB创建语句.sql
@@ -1,5 +1,5 @@
 --------------------------------------
-2、学生表t_student
+--2、学生表t_student
 
 CREATE TABLE t_student (
   uuid varchar(50) NOT NULL,
@@ -14,6 +14,10 @@ CREATE TABLE t_student (
   address varchar(30) DEFAULT NULL,
   remark varchar(30) DEFAULT NULL,
   
+  sex varchar(30) DEFAULT NULL,
+  org varchar(30) DEFAULT NULL,
+  parentRela varchar(30) DEFAULT NULL,
+  
   createDate datetime DEFAULT NULL,
   modifyDate datetime DEFAULT NULL,
   createPeople varchar(50) DEFAULT NULL,
@@ -22,7 +26,7 @@ CREATE TABLE t_student (
   PRIMARY KEY (uuid)
 );
 --------------------------------------
-3、员工表t_employee
+--3、员工表t_employee
 
 CREATE TABLE t_employee (
   uuid varchar(50) NOT NULL,
@@ -32,8 +36,14 @@ CREATE TABLE t_employee (
   depart varchar(30) DEFAULT NULL,
   job varchar(30) DEFAULT NULL,
   permissionTempl varchar(30) DEFAULT NULL,
-  course varchar(30) DEFAULT NULL,
   remark varchar(255) DEFAULT NULL,
+  claTeacher varchar(30) NOT NULL,
+  
+  sex varchar(30) DEFAULT NULL,
+  org varchar(30) DEFAULT NULL,
+  workDate varchar(30) DEFAULT NULL,
+  fullhalf varchar(30) DEFAULT NULL,
+  jobRemark varchar(30) DEFAULT NULL,
   
   createDate datetime DEFAULT NULL,
   modifyDate datetime DEFAULT NULL,
@@ -44,13 +54,14 @@ CREATE TABLE t_employee (
 );
 
 --------------------------------------
-4、课程表t_course
+--4、课程表t_course
 
 CREATE TABLE t_course (
   uuid varchar(50) NOT NULL,
   name varchar(30) NOT NULL,
   category varchar(30) DEFAULT NULL,
   describeA varchar(255) DEFAULT NULL,
+  org varchar(30) DEFAULT NULL,
   
   createDate datetime DEFAULT NULL,
   modifyDate datetime DEFAULT NULL,
@@ -61,20 +72,30 @@ CREATE TABLE t_course (
 );
 
 --------------------------------------
-5、合同表t_contract
+--5、合同表t_contract
 
 CREATE TABLE t_contract (
   uuid varchar(50) NOT NULL,
-  oddNum varchar(30) NOT NULL,
+  cNum varchar(50) NOT NULL,
   stuUuid varchar(50) DEFAULT NULL,
-  campus varchar(30) DEFAULT NULL,
-  account varchar(30) DEFAULT NULL,
-  operator varchar(30) DEFAULT NULL,
-  fee varchar(30) DEFAULT NULL,
-  feeType varchar(30) DEFAULT NULL,
-  feeMode varchar(30) DEFAULT NULL,
-  itemName varchar(30) DEFAULT NULL,
-  amount varchar(30) DEFAULT NULL,
+  cDate varchar(30) DEFAULT NULL,
+  org varchar(30) DEFAULT NULL,
+  totalCount varchar(30) DEFAULT NULL,
+  totalPrice varchar(30) DEFAULT NULL,
+  
+  onePriceA varchar(30) DEFAULT NULL,
+  countA varchar(30) DEFAULT NULL,
+  delPriceA varchar(30) DEFAULT NULL,
+  countGiveA varchar(30) DEFAULT NULL,
+  sumCountA varchar(30) DEFAULT NULL,
+  sumPriceA varchar(30) DEFAULT NULL,
+  
+  onePriceB varchar(30) DEFAULT NULL,
+  countB varchar(30) DEFAULT NULL,
+  delPriceB varchar(30) DEFAULT NULL,
+  countGiveB varchar(30) DEFAULT NULL,
+  sumCountB varchar(30) DEFAULT NULL,
+  sumPriceB varchar(30) DEFAULT NULL,
   
   createDate datetime DEFAULT NULL,
   modifyDate datetime DEFAULT NULL,
@@ -85,7 +106,7 @@ CREATE TABLE t_contract (
 );
 
 --------------------------------------
-6、排课表t_paike_all
+--6、排课表t_paike_all
 
 CREATE TABLE t_paike_all (
   uuid varchar(50) NOT NULL,
@@ -97,6 +118,7 @@ CREATE TABLE t_paike_all (
   keStartTime varchar(30) DEFAULT NULL,
   keLongTime varchar(30) DEFAULT NULL,
   status varchar(30) DEFAULT NULL,
+  weekSome varchar(30) DEFAULT NULL,
   
   createDate datetime DEFAULT NULL,
   modifyDate datetime DEFAULT NULL,
@@ -107,7 +129,7 @@ CREATE TABLE t_paike_all (
 );
 
 --------------------------------------
-7、班级表t_class
+--7、班级表t_class
 
 CREATE TABLE t_class (
   uuid varchar(50) NOT NULL,
@@ -116,6 +138,7 @@ CREATE TABLE t_class (
   classDate varchar(50) DEFAULT NULL,
   status varchar(50) DEFAULT NULL,
   remark varchar(30) DEFAULT NULL,
+  claNum varchar(30) DEFAULT NULL,
   
   org varchar(50) DEFAULT NULL,
   createDate datetime DEFAULT NULL,
@@ -127,7 +150,7 @@ CREATE TABLE t_class (
 );
 
 --------------------------------------
-8、教室表t_classroom
+--8、教室表t_classroom
 
 CREATE TABLE t_classroom (
   uuid varchar(50) NOT NULL,
@@ -144,5 +167,86 @@ CREATE TABLE t_classroom (
   PRIMARY KEY (uuid)
 );
 
+
+--------------------------------------
+
+--9、班级员工表t_class_emp
+
+CREATE TABLE t_class_emp (
+  uuid varchar(50) NOT NULL,
+  classUuid varchar(50) NOT NULL,
+  className varchar(50) DEFAULT NULL,
+  empUuid varchar(50) DEFAULT NULL,
+  empName varchar(50) DEFAULT NULL,
+  
+  PRIMARY KEY (uuid)
+);
+
+--------------------------------------
+
+--10、班级学员表t_class_stu
+
+CREATE TABLE t_class_stu (
+  uuid varchar(50) NOT NULL,
+  classUuid varchar(50) NOT NULL,
+  className varchar(50) DEFAULT NULL,
+  stuUuid varchar(50) DEFAULT NULL,
+  stuName varchar(50) DEFAULT NULL,
+  
+  PRIMARY KEY (uuid)
+);
+
+--------------------------------------
+
+--11、班级课程表t_class_course
+
+CREATE TABLE t_class_course (
+  uuid varchar(50) NOT NULL,
+  classUuid varchar(50) NOT NULL,
+  className varchar(50) DEFAULT NULL,
+  courseUuid varchar(50) DEFAULT NULL,
+  courseName varchar(50) DEFAULT NULL,
+  
+  PRIMARY KEY (uuid)
+);
+
+--------------------------------------
+
+--12、课程员工表t_course_emp
+
+CREATE TABLE t_course_emp (
+  uuid varchar(50) NOT NULL,
+  courseUuid varchar(50) NOT NULL,
+  courseName varchar(50) DEFAULT NULL,
+  empUuid varchar(50) NOT NULL,
+  empName varchar(50) DEFAULT NULL,
+  
+  PRIMARY KEY (uuid)
+);
+
+--------------------------------------
+--13、班级合同表t_class_contract
+
+CREATE TABLE t_class_contract (
+  uuid varchar(50) NOT NULL,
+  classUuid varchar(50) NOT NULL,
+  className varchar(50) DEFAULT NULL,
+  contrUuid varchar(50) DEFAULT NULL,
+  contrName varchar(50) DEFAULT NULL,
+  
+  PRIMARY KEY (uuid)
+);
+
+--------------------------------------
+--14、班级合同表t_userPK
+
+CREATE TABLE t_userPK (
+  uuid varchar(50) NOT NULL,
+  uLogUser varchar(50) NOT NULL,
+  uPassWord varchar(50) DEFAULT NULL,
+  uName varchar(50) DEFAULT NULL,
+  
+  PRIMARY KEY (uuid)
+);
 
 --------------------------------------

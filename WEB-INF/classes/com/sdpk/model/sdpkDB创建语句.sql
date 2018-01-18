03acd45a6811ada031b785cdb7851fd1f198e718
lieo <lieoluntan@163.com>
2018-01-18 13:55:31 +0800

    员工，班级，学生再次提交
    Signed-off-by: lieo <lieoluntan@163.com>

--- WEB-INF/classes/com/sdpk/model/sdpkDB创建语句.sql
+++ WEB-INF/classes/com/sdpk/model/sdpkDB创建语句.sql
@@ -16,7 +16,9 @@ CREATE TABLE t_student (
   
   parentName2 varchar(30) DEFAULT NULL,
   parentPhone2 varchar(30) DEFAULT NULL,
-  address2 varchar(30) DEFAULT NULL,
+  parentRela2 varchar(30) DEFAULT NULL,
+  
+  openAndclose varchar(10) DEFAULT NULL,
   
   sex varchar(30) DEFAULT NULL,
   org varchar(30) DEFAULT NULL,
